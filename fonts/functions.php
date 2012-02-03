1ead1b042eb0d8df6d61f72b7b4f3f29fb704443
saturngod <saturngod@gmail.com>
2012-02-04 02:28:46 +0800

    beta version

---
+++ fonts/functions.php
@@ -0,0 +1,61 @@
+<?php
+function get_browser_name() {
+	$user_agent = $_SERVER['HTTP_USER_AGENT'];
+	if(preg_match("/Firefox/",$user_agent))
+	{
+		return "firefox";
+	}
+	else if(preg_match("/iPhone/",$user_agent))
+	{
+		return "iPhone";
+	}
+	else if(preg_match("/iPad/",$user_agent))
+	{
+		return "iPad";
+	}
+	else if(preg_match("/Android/",$user_agent))
+	{
+		return "android";
+	}
+	else if(preg_match("/Chrome/",$user_agent))
+	{
+		return "chrome";
+	}
+	else if(preg_match("/Safari/",$user_agent))
+	{
+		return "safari";
+	}
+	else if(preg_match("/Opera/",$user_agent))
+	{
+		return "opera";
+	}
+	else if(preg_match("/IEMobile/",$user_agent))
+	{
+		return "iemobile";
+	}
+	else if(preg_match("/MSIE/",$user_agent))
+	{
+		return "ie";
+	}
+}
+
+function mac_os() {
+	$user_agent = $_SERVER['HTTP_USER_AGENT'];
+	if(preg_match("/Mac/",$user_agent))
+	{
+		return true;
+	}
+	return false;
+}
+
+function currentPageURL() {
+ $pageURL = 'http';
+ if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+ $pageURL .= "://";
+ if ($_SERVER["SERVER_PORT"] != "80") {
+  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["PHP_SELF"];
+ } else {
+  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["PHP_SELF"];
+ }
+ return $pageURL;
+}
---
+++ fonts/index.php
@@ -0,0 +1,96 @@
+<?php
+require_once('functions.php');
+$current_url=substr(currentPageURL(),0,-9);
+
+$font_family="Master Piece Uni Sans";
+if(isset($_GET['font']))
+{
+	$font_file=strtolower($_GET['font']);	
+}
+else {
+	$font_file = "masterpiece";
+}
+
+if($font_file=='yunghkio') {
+	$font_file="yunghkio";
+    $font_family="Yunghkio";
+}
+else if($font_file==='myanmar3') {
+	$font_file="myanmar3";
+    $font_family="Myanmar3";
+}
+else if($font_file==='padauk') {
+	$font_file="padauk";
+    $font_family="Padauk";
+}   
+else if($font_file==='mymyanmar') {
+	$font_file="MyMMUnicodeUniversal";
+    $font_family="MyMyanmar Universal";
+}
+
+$browsername = get_browser_name();
+$font_type = "ttf";
+$is_mac = mac_os();
+
+if($browsername=="ie")
+{
+	$font_type = "eot";
+}
+
+if($browsername=="chrome" && $is_mac) {
+	$font_file="myanmar3";
+	$font_family="Myanmar3";
+}
+
+//check and forece Masterpiece if OS is apple related
+if($browsername=='iPhone' or $browsername=='iPad' and $font_file!='myanmar3') {
+	$font_family="Masterpiece Uni Sans";
+    $font_file="masterpiece";
+    $font_type="ttf";
+}
+
+if($is_mac && ($font_file=="padauk" || $font_file=="yunghkio")) {
+	$font_family="Masterpiece Uni Sans";
+	$font_file="masterpiece";
+	$font_type="ttf";
+}
+
+//for android unicode, masterpiece is better
+if($browsername=="android" && $font_file!='zawgyi') {
+	$font_family="Masterpiece Uni Sans";
+	$font_file="masterpiece";
+	$font_type="ttf";
+}
+
+//zawgyi font support all browser
+if($font_file=='zawgyi') {
+	$font_family="Zawgyi-One";
+	$font_file="zawgyi";
+	if($browsername=="ie")
+	{
+		//IE mobile still not support font embed
+		$font_type="eot";
+	}
+	else {
+		$font_type="ttf";
+	}
+}
+
+if($font_type!="")
+{
+	$css ="@font-face {\nfont-family:".$font_family.";";
+
+	$font_path=$current_url."font/".$font_file.".".$font_type;
+
+	if($browsername!="ie") {
+		$css=$css."\nsrc:local('".$font_family."'),";
+		$css=$css."url('".$font_path."');\n}";
+	}
+	else {
+		$css=$css."\nsrc:url('".$font_path."?') format('embedded-opentype');";
+        $css=$css."\n}";
+	}
+	header("Access-Control-Allow-Origin: *");
+	header("Content-Type: text/css");
+	echo $css;
+}
