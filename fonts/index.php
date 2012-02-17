906ed96194c5ed1b57a947c24de6316c2a826f69
Htain Lin Shwe <saturngod@gmail.com>
2012-02-17 16:13:58 +0800

    add config for analytics

--- fonts/index.php
+++ fonts/index.php
@@ -1,30 +1,35 @@
 <?php
+$php_version =(float)phpversion();
+
 date_default_timezone_set('Asia/Singapore');
 require_once('functions.php');
 require_once('src/autoload.php');
-use UnitedPrototype\GoogleAnalytics;
+require_once('../config.php');
+use UnitedPrototype\GoogleAnalytics;//if you are using PHP4, remove this line
 
-// Initilize GA Tracker
-$tracker = new GoogleAnalytics\Tracker('UA-2358448-29', 'mmwebfonts.comquas.com');
+if(ENABLE_ANALYTICS &&  $php_version>=5.0) {
 
-// Assemble Visitor information
-// (could also get unserialized from database)
-$visitor = new GoogleAnalytics\Visitor();
-$visitor->setIpAddress($_SERVER['REMOTE_ADDR']);
-$visitor->setUserAgent($_SERVER['HTTP_USER_AGENT']);
-$visitor->setScreenResolution('1024x768');
+	// Initilize GA Tracker
+	$tracker = new GoogleAnalytics\Tracker(ANALYTICS_ID, ANALYTICS_DOMAIN);
 
-// Assemble Session information
-// (could also get unserialized from PHP session)
-$session = new GoogleAnalytics\Session();
+	// Assemble Visitor information
+	// (could also get unserialized from database)
+	$visitor = new GoogleAnalytics\Visitor();
+	$visitor->setIpAddress($_SERVER['REMOTE_ADDR']);
+	$visitor->setUserAgent($_SERVER['HTTP_USER_AGENT']);
+	$visitor->setScreenResolution('1024x768');
 
-// Assemble Page information
-$page = new GoogleAnalytics\Page('/fonts/index.php');
-$page->setTitle('mmwebfonts');
+	// Assemble Session information
+	// (could also get unserialized from PHP session)
+	$session = new GoogleAnalytics\Session();
 
-// Track page view
-$tracker->trackPageview($page, $session, $visitor);
+	// Assemble Page information
+	$page = new GoogleAnalytics\Page('/fonts/index.php');
+	$page->setTitle('mmwebfonts');
 
+	// Track page view
+	$tracker->trackPageview($page, $session, $visitor);
+}
 
 
 //start page
@@ -115,10 +120,11 @@ if($font_file=='zawgyi') {
 
 if($font_type!="")
 {
+	if(ENABLE_ANALYTICS &&  $php_version>=5) {
+		$event = new GoogleAnalytics\Event("fonts",$font_family,$font_type);
 
-	$event = new GoogleAnalytics\Event("fonts",$font_family,$font_type);
-
-	$tracker->trackEvent($event,$session,$visitor);
+		$tracker->trackEvent($event,$session,$visitor);
+	}
 
 	$css ="@font-face {\nfont-family:".$font_family.";";
 
@@ -130,8 +136,9 @@ if($font_type!="")
 	}
 	else {
 		$css=$css."\nsrc:url('".$font_path."?') format('embedded-opentype');";
-        $css=$css."\n}";
+        		$css=$css."\n}";
 	}
+
 	header("Access-Control-Allow-Origin: *");
 	header("Content-Type: text/css");
 	echo $css;
