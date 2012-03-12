633ed6deda4642332be2ca3eb6c46fe3b907da48
Htain Lin Shwe <saturngod@gmail.com>
2012-03-12 16:27:33 +0800

    fixed zawgyi

---
+++ README.md
@@ -0,0 +1,10 @@
+Requirement
+==========
+
+* Need PHP 5 for using google analytics
+* google analytics for tracking how many users request per day
+
+For PHP 4
+=======
+
+* remove **use UnitedPrototype\GoogleAnalytics;** in line 8 at fonts/index.php
---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Google Analytics is for tracking how many user are using MMWebFonts
+* and what fonts are the most usages
+* can create account at http://www.google.com/analytics/
+* if you want to use Google Analytics , need PHP 5.0 or later
+ **/;
+define("ENABLE_ANALYTICS",true);
+define("ANALYTICS_ID","UA-2358448-29");
+define("ANALYTICS_DOMAIN","mmwebfonts.comquas.com");
